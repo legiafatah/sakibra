3ac442bd49f8f822ed2d3cea837896b52747b2ad
Legi Afatah <legi.gaya@gmail.com>
2025-07-10 08:47:06 +0700

    API3

--- routes/api.php
+++ routes/api.php
@@ -42,10 +42,15 @@ Route::post('/pelanggaran', function (Request $request) {
     try {
         $filename = time() . '_' . $request->filename;
 
-        $path = storage_path("app/public/bukti/{$filename}");
+         $publicPath = public_path("bukti/{$filename}");
+
+        // Buat folder jika belum ada
+        if (!file_exists(public_path('bukti'))) {
+            mkdir(public_path('bukti'), 0755, true);
+        }
         $decoded = base64_decode($request->gambar_base64);
 
-        file_put_contents($path, $decoded);
+        file_put_contents($publicPath, $decoded);
 
         BuktiPelanggaran::create([
             'image' => $filename,
